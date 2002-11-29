58db6566ca0048732555fae23f86569c2b0c7a97
Arnaud Launay <asl@launay.org>
2002-11-29 16:09:58 +0000

    <config.h> -> "config.h"
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphoto2@5859 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2/gphoto2-cmd-capture.c
+++ gphoto2/gphoto2-cmd-capture.c
@@ -17,7 +17,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-#include <config.h>
+#include "config.h"
 #include "gphoto2-cmd-capture.h"
 
 #include <stdlib.h>
