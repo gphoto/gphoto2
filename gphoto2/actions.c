bba1d99ae727bf920445610d8c63d2578e22448a
Hans Ulrich Niedermann <gp@n-dimensional.de>
2003-06-05 21:05:07 +0000

    it's 2003 now and there have been changes
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphoto2@6476 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2/actions.c
+++ gphoto2/actions.c
@@ -762,7 +762,7 @@ print_version_action (GPParams *p)
 	int n;
 	printf (_("gphoto2 %s\n"
 		  "\n"
-		  "Copyright (c) 2000-2002 Lutz Mueller and others\n"
+		  "Copyright (c) 2000-2003 Lutz Mueller and others\n"
 		  "%s"
 		  "\n"
 		  "gphoto2 comes with NO WARRANTY, to the extent permitted by law. You may\n"
