d4af961cd8fdfd6d8da68aa2e518ad177eb022aa
Hans Ulrich Niedermann <gp@n-dimensional.de>
2006-08-16 20:41:26 +0000

    remove non-popt cases
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphoto2@9081 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2/globals.h
+++ gphoto2/globals.h
@@ -1,21 +1,12 @@
 #ifndef __GLOBALS_H__
 #define __GLOBALS_H__
 
-#ifndef HAVE_POPT
-#  include "main.h"
-#endif
-
 extern char	glob_cancel;
 
 extern char	*glob_folder;
 
 extern char	glob_cwd[];
 extern int	glob_option_count;
-
-#ifndef HAVE_POPT
-extern Option option[];
-extern int    glob_option_count;
-#endif
 
 #endif
 
