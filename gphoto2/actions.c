f67b531e23780dea29a1e92226c5b7a3f44b7a07
Marcus Meissner <marcus@jet.franken.de>
2007-09-19 05:38:50 +0000

    year is 2007
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphoto2@10717 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto2/actions.c
+++ gphoto2/actions.c
@@ -891,7 +891,7 @@ print_version_action (GPParams __unused__ *p)
 		  "\n"
 		  "This version of gphoto2 is using the following software versions and options:\n"),
 		VERSION,
-		2006, /* year of release! */
+		2007, /* year of release! */
 		port_message
 		);
 
