cd4d883d9c87856caf286f34ffcdffc826635d0a
Marcus Meissner <marcus@jet.franken.de>
2017-04-18 08:46:18 +0200

    removed duplicate trigger-capture entry
    fixes https://github.com/gphoto/gphoto2/issues/65

--- gphoto2/main.c
+++ gphoto2/main.c
@@ -2068,8 +2068,6 @@ main (int argc, char **argv, char **envp)
 		 ARG_CAPTURE_SOUND, N_("Capture an audio clip"), NULL},
 		{"capture-tethered", '\0', POPT_ARG_STRING|POPT_ARGFLAG_OPTIONAL, NULL,
 		 ARG_CAPTURE_TETHERED, N_("Wait for shutter release on the camera and download"), N_("COUNT, SECONDS, MILLISECONDS or MATCHSTRING")},
-		{"trigger-capture", '\0', POPT_ARG_NONE, NULL,
-		 ARG_TRIGGER_CAPTURE, N_("Trigger image capture"), NULL},
 		POPT_TABLEEND
 	};
 	const struct poptOption fileOptions[] = {
